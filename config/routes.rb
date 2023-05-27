24dce88f525028091cec7db1113d7bfe38a3bfb0
Alex Smirnov <sas2job@gmail.com>
2023-05-27 18:28:11 +0300

    Add route for notes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'notes/index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :notes, only: [:index]
+  root to: 'notes#index'
 end
