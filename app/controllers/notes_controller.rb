e40b5da83697176d36e75d3a6fdb22b060ab346a
Alex Smirnov <sas2job@gmail.com>
2023-05-28 08:27:47 +0300

    Add index & create for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,32 @@
 class NotesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_note, only: [:destroy]
+
   def index
+    @notes = Note.order(:line_number).page(params[:page]).per(20)
+    @note = Note.new
+  end
+
+  def create
+    @note = current_user.notes.new(note_params)
+    if @note.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @note.destroy
+  end
+
+  private
+
+  def set_note
+    @note = current_user.notes.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:note)
   end
 end
